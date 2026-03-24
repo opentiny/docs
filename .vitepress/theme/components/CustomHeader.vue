8ac229b63a68e3f75f054957bed7fbdd43b8ce88
wuyiping <42107997+wuyiping0628@users.noreply.github.com>
2026-03-24 17:11:45 +0800

    fix: resolve the 404 issue with mobile icons (#91)

--- .vitepress/theme/components/CustomHeader.vue
+++ .vitepress/theme/components/CustomHeader.vue
@@ -119,8 +119,8 @@
               <img
                 :src="
                   isDark
-                    ? `${prefix}logo-opentiny-next-text-dark.svg`
-                    : `${prefix}logo-opentiny-next-text.svg`
+                    ? `${prefix}images/logo-opentiny-next-text-dark.svg`
+                    : `${prefix}images/logo-opentiny-next-text.svg`
                 "
                 alt="OpenTiny NEXT"
                 class="logo-icon"
